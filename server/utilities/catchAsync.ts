b93a1d7ddabea2cae7189d20f8399348367f4709
sharmaarpan143 <sharmaarpan143@gmail.com>
2024-06-30 22:53:06 +0530

    ye to chalega hi 4

--- server/utilities/catchAsync.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express-serve-static-core';
-
-const catchAsync = (fn: Function) => {
-    return (req: Request, res: Response, next: NextFunction) =>
-        fn(req, res, next).catch(next);
-};
-
-export default catchAsync;
