90a050c668e9b975cf7c0e04098b69ebf2427063
sharmaarpan143 <sharmaarpan143@gmail.com>
2024-08-10 01:40:00 +0530

    main

--- server/utilities/multer.js
+++ server/utilities/multer.js
@@ -1,3 +1,6 @@
 import multer from 'multer';
-export const upload = multer({ storage: multer.memoryStorage() });
+export const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: 20 * 1024 * 1024,
+});
 //# sourceMappingURL=multer.js.map
