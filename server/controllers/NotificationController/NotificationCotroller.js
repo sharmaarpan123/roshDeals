c2784b26c866919bae7da0fd3452f950da983dfb
sharmaarpan143 <sharmaarpan143@gmail.com>
2024-09-03 22:22:43 +0530

    notification moduel done

--- server/controllers/NotificationController/NotificationCotroller.js
+++ server/controllers/NotificationController/NotificationCotroller.js
@@ -1,33 +1,55 @@
+import Order from '../../database/models/Order.js';
 import User from '../../database/models/User.js';
 import catchAsync from '../../utilities/catchAsync.js';
-import { successResponse } from '../../utilities/Responses.js';
+import { errorResponse, successResponse } from '../../utilities/Responses.js';
 import { sendNotification } from '../../utilities/sendNotification.js';
 import { sendNotificationSchema } from './schema.js';
 
 export const sendNotificationController = catchAsync(async (req, res) => {
     const data = sendNotificationSchema.parse(req.body);
 
-    if (data.type === 'all') {
-        const tokens = await User.find({}, { _id: 0, fcmToken: 1 });
-
-        sendNotification({
+    const messageBody = {
+        notification: {
+            body: data.body,
+            title: data.title,
+            imageUrl: `${process.env.BASE_URL}/images/logo.jpeg`,
+        },
+        android: {
             notification: {
-                body: data.body,
-                title: data.title,
                 imageUrl: `${process.env.BASE_URL}/images/logo.jpeg`,
             },
-            android: {
-                notification: {
-                    imageUrl: `${process.env.BASE_URL}/images/logo.jpeg`,
-                },
-            },
-            tokens: tokens.map((item) => item.fcmToken),
-        });
-    }
+        },
+    };
 
-    return res.status(200).json(
-        successResponse({
-            message: 'notification sended successfully',
-        }),
-    );
+    if (data.type === 'all') {
+        const tokens = await User.find({}, { _id: 0, fcmToken: 1 });
+        messageBody.tokens = tokens.map((item) => item.fcmToken);
+        sendNotification(messageBody);
+        return res.status(200).json(
+            successResponse({
+                message: 'notification sended successfully',
+            }),
+        );
+    }
+    if (data.type === 'dealOrderStatus') {
+        const tokens = await Order.find({
+            orderFormStatus: data.orderStatus,
+        }).populate({ path: 'userId', select: 'fcmToken' });
+
+        if (!tokens.length) {
+            return res.status(400).json(
+                errorResponse({
+                    message: 'No order found with this status',
+                }),
+            );
+        }
+
+        messageBody.tokens = tokens.map((item) => item?.userId?.fcmToken);
+        sendNotification(messageBody);
+        return res.status(200).json(
+            successResponse({
+                message: 'notification sended successfully',
+            }),
+        );
+    }
 });
