1b3f9caab491a4830da051a3beb194d145bce2a5
Yash Chouhan <yashchouhan@Yashs-MacBook-Air.local>
2025-03-23 15:29:15 +0530

    fixes

--- server/controllers/AdminController/SubAdmin/SubAdminController.js
+++ server/controllers/AdminController/SubAdmin/SubAdminController.js
@@ -323,7 +323,7 @@ class subAdminController {
                 ...req?.body,
                 adminId: isSuperAdminId
                     ? req?.body?.adminId
-                    : getAccessorId(adminId),
+                    : getAccessorId(req?.body?.adminId),
             });
 
         const updatedData = await AdminSubAdminLinker.findOneAndUpdate(
