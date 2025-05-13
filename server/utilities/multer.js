b0ac1be3ee2652f228e18dba8cf8148e444ceb52
Arpan sharma <sharmaarpan143@gmail.com>
2025-05-14 00:11:36 +0530

    working..

--- server/utilities/multer.js
+++ server/utilities/multer.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 export const upload = multer({
     // storage: multer.memoryStorage(),
-    // dest: "uploads/",
-    dest: path.join('/tmp', 'uploads'),
+    dest: "uploads/",
+    // dest: path.join('/tmp', 'uploads'),
     limits: 4 * 1024 * 1024,
 });
 //# sourceMappingURL=multer.js.map
